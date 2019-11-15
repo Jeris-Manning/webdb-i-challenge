5315d997b164b58a3f504033b54bb29530b36616
Jeris Manning <the.goose.moose@gmail.com>
2019-11-14 17:25:04 -0700

    medium commit

---
+++ data/accountsDb.js
@@ -0,0 +1,37 @@
+const db = require('./dbConfig.js');
+
+module.exports = {
+  get,
+  getById,
+  insert,
+  update,
+  remove,
+};
+
+function get() {
+  return db('accounts');
+}
+
+function getById(id) {
+  return db('accounts')
+    .where({ id })
+    .first();
+}
+
+function insert(account) {
+  return db('accounts')
+    .insert(account)
+   
+}
+
+function update(id, changes) {
+  return db('accounts')
+    .where({ id })
+    .update(changes);
+}
+
+function remove(id) {
+  return db('accounts')
+    .where('id', id)
+    .del();
+}
--- server.js
+++ server.js
@@ -1,9 +1,42 @@
 const express = require('express');
 
-const db = require('./data/dbConfig.js');
+const Accounts = require('./data/accountsDb.js');
 
 const server = express();
 
 server.use(express.json());
+
+server.get('/', async (req, res) => {
+  try {
+    const accounts = await Accounts.get();
+    res.status(200).json(accounts);
+  } catch (err) {
+    res.status(500).json({ success: false, message: "Server didn't work =(" });
+  }
+});
+
+server.get('/:id', async (req, res) => {
+  try {
+    const id = req.params.id;
+    const account = await Accounts.getById(id);
+    res.status(200).json(account);
+  } catch (err) {
+    res.status(500).json({ success: false, message: "Server didn't work =(" });
+  }
+});
+
+server.post('/', (req, res) => {
+ try{
+
+const newAccount = req.body;
+
+const accountAdded = await Accounts.insert(newAccount);
+
+res.status(200).json(accountAdded);
+
+ }catch (err){
+  res.status(500).json({ success: false, message: "Server didn't work =(" });
+ }
+});
 
 module.exports = server;
